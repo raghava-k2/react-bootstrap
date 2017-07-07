20097dd53e29f74196ffb50c67936882e6554c51
vijaya,kukapalli <vijaya.kukapalli@adp.com>
2017-07-07 19:50:02 +0530

    Added dialog details

--- src/components/schedulerCom/CreateJob.js
+++ src/components/schedulerCom/CreateJob.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types'
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
+import Checkbox from 'material-ui/Checkbox';
+import Toggle from 'material-ui/Toggle';
+import TextField from 'material-ui/TextField';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 
 export default class CreateJob extends Component {
     static propTypes = {
@@ -17,6 +24,12 @@ export default class CreateJob extends Component {
         this.handleSubmit = this
             .handleSubmit
             .bind(this)
+        this.styles = {
+            root: {
+                display: 'flex',
+                flexWrap: 'wrap'
+            }
+        }
     }
 
     handleClose() {
@@ -51,14 +64,58 @@ export default class CreateJob extends Component {
         return (
             <div className='jobDialog'>
                 <Dialog
-                    title="Scrollable Dialog"
+                    title="Create Job"
                     actions={actions}
                     modal={false}
                     open={this.props.show}
                     onRequestClose={this.handleClose}
                     autoScrollBodyContent={true}>
-                    <h1>Inside dialog
-                    </h1>
+                    <div style={this.styles.root}>
+                        <section>
+                            <List>
+                                <Subheader>Job Details</Subheader>
+                                <TextField
+                                    hintText="ex:orpan job"
+                                    errorText="This field is required"
+                                    floatingLabelText="Enter Job Name"/><br/>
+                                <TextField
+                                    hintText="ex:daily runs"
+                                    errorText="This field is required"
+                                    floatingLabelText="Enter Job Group Name"/>
+                            </List>
+                            <Divider/>
+                            <List>
+                                <Subheader>Hangout Notifications</Subheader>
+                                <RadioButtonGroup name="shipSpeed" defaultSelected="not_light">
+                                    <RadioButton value="daily" label="Daily"/>
+                                    <RadioButton value="weekly" label="Weekly"/>
+                                    <RadioButton value="monthly" label="Monthly"/>
+                                    <RadioButton value="yearly" label="Yearly"/>
+                                </RadioButtonGroup>
+                            </List>
+                        </section>
+                        <section>
+                            <List>
+                                <ListItem
+                                    primaryText="When calls and notifications arrive"
+                                    secondaryText="Always interrupt"/>
+                            </List>
+                            <Divider/>
+                            <List>
+                                <Subheader>Priority Interruptions</Subheader>
+                                <ListItem primaryText="Events and reminders" rightToggle={< Toggle />}/>
+                                <ListItem primaryText="Calls" rightToggle={< Toggle />}/>
+                                <ListItem primaryText="Messages" rightToggle={< Toggle />}/>
+                            </List>
+                            <Divider/>
+                            <List>
+                                <Subheader>Hangout Notifications</Subheader>
+                                <ListItem primaryText="Notifications" leftCheckbox={< Checkbox />}/>
+                                <ListItem primaryText="Sounds" leftCheckbox={< Checkbox />}/>
+                                <ListItem primaryText="Video sounds" leftCheckbox={< Checkbox />}/>
+                            </List>
+                        </section>
+                    </div>
                 </Dialog>
             </div>
         );
